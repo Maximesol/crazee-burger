85a96d07485dbca84f15106b4090d6eb1100d985
Maximesol <maxime.gognies@gmail.com>
2023-08-08 11:39:37 +0200

    💄 F03: Add Logo style + css

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -18,26 +18,17 @@ export default function LoginPage() {
 }
 
 
+
+
 const LoginPageStyled = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  align-items:center;
+  align-items: center;
   height: 100vh;
-
-  ::before {
-    content: "";
-    position: absolute; // Positionnement absolu pour couvrir tout le conteneur
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("public/images/F03 burger-background.jpg");
-    background-size: cover;
-    background-position: center;
-    
-    
-    z-index: -1; // Mettre en arrière-plan
-  }
-
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("images/F03 burger-background.jpg");
+  background-size: cover;
+  background-position: center;
+  margin: 0;
+  padding: 0;
 `;
--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -1,13 +1,42 @@
 import React from 'react'
 import styled from 'styled-components'
 
+
 export default function Logo() {
-    return (
-        <LogoStyled>Logo</LogoStyled>
-    )
+  return (
+    <LogoStyled>
+      <h1>CRAZEE</h1>
+      <img src="/public/images/logo-burger.png" />
+      <h1>BURGER</h1>
+
+    </LogoStyled>
+  )
 }
 
 const LogoStyled = styled.div`
-  background: blue;
+  display:flex;
+  align-items: center;
+  transform: scale(2.5);
+  img {
+    object-fit: contain;
+    object-position: center;
+    height: 60px;
+    width:80px
+    margin: 0 5px
+  }
+  h1 {
+    display:flex;
+    align-items: center;
+    color: #FFA01B;
+    text-align: center;
+    font-family: Amatic SC, cursive;
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1em;
+    letter-spacing: 1.5px;
+    text-transform: uppercase;
+
+  }
 `;
 
