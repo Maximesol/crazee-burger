b98bc1a814bce476ee2d125fc309be929a5d508d
Maximesol <maxime.gognies@gmail.com>
2023-08-17 10:43:14 +0200

    ♻️ F06: Order > NavBar > Add <ToastAdmin />

--- src/components/pages/order/NavBar/RightSide.jsx
+++ src/components/pages/order/NavBar/RightSide.jsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 import ToggleButton from '../../../reusable-ui/ToggleButton';
 import Profile from "./Profile"
 import { useState } from 'react';
-import { ToastContainer, toast } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-import { theme } from '../../../../theme';
+import { toast } from "react-toastify"
+import ToastAdmin from './ToastAdmin';
+
 
 
 
@@ -16,7 +16,7 @@ export default function RightSide({ username }) {
 
     //comportement
     const handleToggle = () => {
-        setIsAdmin((prev) => !prev)
+
         if (!isAdmin) {
             toast.info("Mode admin activé", {
                 // icon: <FaUserSecret size={30} />,
@@ -30,12 +30,13 @@ export default function RightSide({ username }) {
                 progress: undefined,
             })
         }
+        setIsAdmin((prev) => !prev)
     }
 
     //affichage
     return (
         <RightSideStyled >
-            <ToastContainer className="toaster" bodyClassName="body-toast" />
+            <ToastAdmin />
             <ToggleButton
                 isChecked={isAdmin}
                 onToggle={handleToggle}
@@ -52,23 +53,6 @@ const RightSideStyled = styled.div`
   
         display: flex;
         align-items: center;
-        padding-right: 50px;
-        .toaster {
-        max-width: 300px;
-  }
-
-  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
-    background: ${theme.colors.background_dark};
-  }
-
-  .body-toast {
-    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
-      margin-right: 20px;
-      margin-left: 5px;
-    }
-    div {
-      line-height: 1.3em;
-    }
-  }
+        padding-right: 50px;      
     
 `;
