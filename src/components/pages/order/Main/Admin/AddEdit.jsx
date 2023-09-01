e33fde68de441724ed31ae7bc854eb1840104c70
Maximesol <maxime.gognies@gmail.com>
2023-09-01 16:51:54 +0200

    💄 F09: Admin > AddEdit > Add hint message

--- src/components/pages/order/Main/Admin/AddEdit.jsx
+++ src/components/pages/order/Main/Admin/AddEdit.jsx
@@ -1,11 +1,38 @@
 
 import styled from 'styled-components';
+import { HiCursorClick } from "react-icons/hi"
+
+
 export default function AddEdit() {
     return (
-        <AddEditStyled>AddEdit</AddEditStyled>
+        <AddEditStyled>
+            <div className='hint-message-icon'>
+                <span className='hint-message'>Cliquer sur un produit du menu pour le modifier</span>
+                <HiCursorClick className='icon' />
+            </div>
+        </AddEditStyled>
     )
 }
 
 const AddEditStyled = styled.form`
+
+.hint-message-icon{
+    display: flex;
+    align-items: center;
+    font-family: Amatic SC;
+    font-size: 24px;
+    color: #747B91;
+    position: relative;
+    top:50 px;
+}
+
+.hint-message {
+    margin-right: 10px;
+}
+
+.icon { 
+    width: 24px;
+    height: 24px;
+}
   
 `;
