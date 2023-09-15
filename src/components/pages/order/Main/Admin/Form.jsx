61c0136aa0df15a43901d9e909cb0fbc8a91d262
Maximesol <maxime.gognies@gmail.com>
2023-09-15 08:24:08 +0200

    ♻️ F09: AddForm > add <Form>

---
+++ src/components/pages/order/Main/Admin/Form.jsx
@@ -0,0 +1,83 @@
+import styled from 'styled-components'
+import TextInput from '../../../../reusable-ui/TextInput'
+import ImagePreview from './ImagePreview'
+import { getinputTextsConfig } from './inputTexts.Config'
+import React from 'react'
+
+
+
+export default function Form({ onSubmit, onChange, product, children }) {
+
+    // state
+
+    // comportements 
+
+
+
+
+
+    const inputTexts = getinputTextsConfig(product)
+
+
+    return (
+        <FormStyled onSubmit={onSubmit}>
+
+            <ImagePreview imageSource={product.imageSource} title={product.title} />
+
+            <div className='div-input'>
+
+                {inputTexts.map((input => {
+                    return (
+                        <TextInput
+                            key={input.id}
+                            {...input}
+                            onChange={onChange}
+                            version="minimalist"
+                        />
+                    )
+                }))}
+
+
+            </div>
+            {children} {/* le bouton d'ajout */}
+
+        </FormStyled>
+    )
+}
+
+
+const FormStyled = styled.form`
+
+display:grid;
+box-sizing: border-box;
+grid-template-columns: 1fr 3fr;
+grid-template-rows: repeat(4, 1fr);
+width: 880px;
+height: 160px;
+grid-column-gap: 20px;
+grid-row-gap: 8px;
+padding-left: 70px;
+
+input{
+    background: #F5F5F7;
+    border: none;
+    width: 100%;
+  
+}
+input::placeholder {
+    color: #A7A8AD; 
+}
+
+.div-input{
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, 1fr);
+    grid-area: 1 / 2 / 4 / 4;
+    gap: 5px;
+    max-width: 100%;
+    grid-row-gap: 8px;
+       
+}
+
+  
+`;
