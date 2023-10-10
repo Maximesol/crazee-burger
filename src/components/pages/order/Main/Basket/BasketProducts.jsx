959d45ea1a427e4348a22bd8d8bbca74e7e936f0
Maximesol <maxime.gognies@gmail.com>
2023-10-10 16:58:39 +0200

    🚧 F12: BasketProducts change logic for layout in BasketCards

--- src/components/pages/order/Main/Basket/BasketProducts.jsx
+++ src/components/pages/order/Main/Basket/BasketProducts.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import styled from 'styled-components';
 import BasketCard from './BasketCard';
+import AdminContext from "../../../../../contexts/AdminContext.jsx"
+import { useContext } from 'react';
 
 const COMING_SOON = "/public/images/coming-soon.png"
 
 
 export default function BasketProducts({ basket, isAdmin, handleDeleteBasketProduct }) {
+
+  const { menu } = useContext(AdminContext)
 
   const handleDelete = (id) => {
     handleDeleteBasketProduct(id)
@@ -13,15 +17,20 @@ export default function BasketProducts({ basket, isAdmin, handleDeleteBasketProd
 
   return (
     <BasketProductsStyled>
-      {basket.map((product) => (
-        <div className='basket-card' key={product.id}>
-          <BasketCard
-            {...product}
-            imageSource={product.imageSource ? product.imageSource : COMING_SOON}
-            isClickable={isAdmin}
-            onDelete={() => handleDelete(product.id)} />
-        </div>
-      ))}
+      {basket.map((product) => {
+        const menuProduct = menu.find(({ id }) => id === product.id)
+        return (
+          <div className='basket-card' key={product.id}>
+            <BasketCard
+              {...menuProduct}
+              imageSource={menuProduct.imageSource ? menuProduct.imageSource : COMING_SOON}
+              isClickable={isAdmin}
+              quantity={product.quantity}
+              onDelete={() => handleDelete(product.id)}
+            />
+          </div>
+        )
+      })}
     </BasketProductsStyled>
   )
 }
