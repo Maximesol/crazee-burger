1de3fdf15aab641134f00848102b504ced8ebeac
Maximesol <maxime.gognies@gmail.com>
2023-10-19 15:05:16 +0200

    ✨ F14: Basket > Header > Add Casino Effect on total price

--- src/components/reusable-ui/CasinoEffect.jsx
+++ src/components/reusable-ui/CasinoEffect.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
+import { styled } from 'styled-components';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 
 export default function CasinoEffect({ count }) {
     return (
-        <div>
-            <span>{count}</span>
+        <TransitionGroup className={"transition-group"} component={CasinoEffectStyled}>
 
-        </div>
+            <CSSTransition
+                classNames={"count-animated"}
+                timeout={1000}
+                key={count}>
+                <span>{count}</span>
+            </CSSTransition>
+
+        </TransitionGroup>
     )
 }
+
+const CasinoEffectStyled = styled.div`
+
+position: relative;
+overflow-y: hidden;
+
+
+span {
+    display: inline-block;
+}
+
+.count-animated-enter {
+    transform: translateY(100%);
+
+}
+.count-animated-enter-active {
+    transition: 300ms;
+    transform: translateY(0%);
+
+}
+
+.count-animated-exit {
+    
+    transform: translateY(0%);
+    position: absolute;
+    right: 0;
+    bottom: 0;
+
+}
+.count-animated-exit-active {
+    transform: translateY(-100%);
+    transition: 300ms;
+
+}
+
+
+  
+`;
